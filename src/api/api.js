ac76a556d60afa4f76e0050af284d4608d56f604
santhosh subramaniam <121019934+Santhosh-Subramaniam@users.noreply.github.com>
2024-06-20 15:02:24 +0530

    final code

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '360221b293f7b2e2e10fddfe1b5cc4b2';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
